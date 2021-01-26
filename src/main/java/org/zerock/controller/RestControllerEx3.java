2ddf6bb1117f9710b13127af2554b79f78ebf5f6
pedroyoung95 <jty9531@gmail.com>
2021-01-26 12:52:28 +0900

    1/26 12:52 수업내용

--- src/main/java/org/zerock/controller/RestControllerEx3.java
+++ src/main/java/org/zerock/controller/RestControllerEx3.java
@@ -1,8 +1,10 @@
 package org.zerock.controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.Rest1;
 
 import lombok.extern.log4j.Log4j;
 
@@ -34,10 +36,25 @@ public class RestControllerEx3 {
 	}
 	
 	@RequestMapping("/ex4/{val}/other/{age}")
-	//파리미터에 타입에 맞게 URL 경로의 일부를 자동으로 변환해줌
-	//{}괄호 안 명칭과 파라미터 이름이 같으면 @PathVariable의 파리미터 설정 필요X
+	//파리미터에 타입에 맞게 URL 경로의 일부를 자동으로 형변환해줌
+	//{}괄호 안 명칭과 파라미터 이름이 같으면 자동으로 할당
 	public String method4(@PathVariable String val, @PathVariable int age) {
 		log.info("method4");
 		return val + age;
+	}
+	
+	@RequestMapping("/ex5")
+	public String method5(@RequestBody String b) {
+//@RequestBody : 전달된 요청(request)의 내용(body)를 파라미터로 변환
+		log.info(b);
+		return "method5";
+	}
+	
+	@RequestMapping("/ex6")
+	public String method6(@RequestBody Rest1 body) {
+//JSON타입의 data를 객체에 맞게 자동으로 값을 할당해줌
+		log.info(body);
+		log.info(body.getName());
+		return "method6";
 	}
 }
