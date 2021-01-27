4abced95a9c68db4bf34c67941291e8c855f4840
pedroyoung95 <jty9531@gmail.com>
2021-01-27 09:45:53 +0900

    1/27 09:43 수업내용

--- src/main/java/org/zerock/controller/ReplyController.java
+++ src/main/java/org/zerock/controller/ReplyController.java
@@ -1,7 +1,13 @@
 package org.zerock.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.ReplyVO;
 import org.zerock.service.ReplyService;
 
 import lombok.AllArgsConstructor;
@@ -14,4 +20,23 @@ import lombok.extern.log4j.Log4j;
 public class ReplyController {
 
 	private ReplyService service;
+	
+	@PostMapping(path = "/new",
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.TEXT_PLAIN_VALUE)
+	public ResponseEntity<String> create(@RequestBody ReplyVO reply) {
+		log.info("reply : " + reply);
+		
+		int insertCount = service.register(reply);
+		
+		log.info("insertCount : " + insertCount);
+		
+		if(insertCount == 1) {
+			return new ResponseEntity<>("success", HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	
 }
