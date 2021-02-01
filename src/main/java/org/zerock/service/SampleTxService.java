73cc789e15e7cb845e3aacfc22a9a856162f4bb5
pedroyoung95 <jty9531@gmail.com>
2021-02-01 21:49:10 +0900

    2/1 21:49 수업내용

--- src/main/java/org/zerock/service/SampleTxService.java
+++ src/main/java/org/zerock/service/SampleTxService.java
@@ -20,7 +20,7 @@ public class SampleTxService {
 	private Sample2Mapper mapper2;
 	
 	@Transactional
-//쪼개질 수 업는 하나의 단위 작업 = 트랜잭션
+//쪼개질 수 없는 하나의 단위 작업 = 트랜잭션
 //하나의 트랜잭션으로 관리하고자 하는 메소드에 @Transactional 붙이기
 //root-context.xml에 transactionManager bean객체 등록이 필요할 수도 있음
 //원자성 원칙:트랜잭션이 A,B로 구성되어 있으면 두 작업 중 하나만 실패해도 원점으로 회복
