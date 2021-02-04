a804d8e76a84e76d683e08876aea9817d2f615e7
pedroyoung95 <jty9531@gmail.com>
2021-02-04 17:23:57 +0900

    2/4 17:23 java web project

---
+++ src/main/java/org/zerock/domain/MemberVO.java
@@ -0,0 +1,12 @@
+package org.zerock.domain;
+
+import java.util.Date;
+
+public class MemberVO {
+
+	private String id;
+	private String password;
+	private String name;
+	private Date signDate;
+	private int completeCnt;
+}
---
+++ src/main/java/org/zerock/mapper/MemberMapper.java
@@ -0,0 +1,18 @@
+package org.zerock.mapper;
+
+import org.zerock.domain.MemberVO;
+
+public interface MemberMapper {
+
+	public void signin(MemberVO member);
+	
+	public int signout(String id);
+	
+	public MemberVO read(String id);
+	
+	public int changeName(String id);
+	
+	public int changePassword(String id);
+	
+	public void updateCompleteCnt(String id);
+}
